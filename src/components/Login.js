bec0f233724ea86cd192c5b3a375963330d1e6ac
Roxin Liu <rliu227@wisc.edu>
2020-10-22 23:26:41 -0500

    Frontend setup

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { 
+    Typography 
+} from "@material-ui/core/";
+
+export default function Home() {
+    return (
+        <Typography>
+            Login to Find My Course.
+        </Typography>
+    )
+}
