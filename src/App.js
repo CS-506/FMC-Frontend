ddbf5566d14ccff7b416c5ef9eef970c0c8f976b
Jerry Yu <jyu283@wisc.edu>
2020-10-24 18:41:48 -0500

    PROJSTART: working ver. w/ Home View routing
    - Removed unnecessary .css files.
    - material-ui packages were preventing react from compiling
      successfully, removed until further investigation.
    
    	deleted:    src/App.css
    	modified:   src/App.js
    	deleted:    src/components/Home.js
    	new file:   src/components/HomeView.js
    	deleted:    src/components/Login.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import './App.css';
+/**
+ * FMC-frontend/src/App.js
+ * 
+ * Manages mapping from url address to page components.
+ * 
+ * Author(s):
+ * 		Jerry Yu, Roxin Liu
+ */
+
+import React from "react";
 import {
-  BrowserRouter as Router,
-  Route,
+	BrowserRouter as Router,
+	Route,
 } from "react-router-dom";
-import Home from "./components/Home"
-import Login from "./components/Login"
+
+import HomeView from "./components/HomeView"
 
 export default function App() {
-  return (
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-      </Router>
-    
-  );
+	return (
+		<Router>
+			<Route
+				exact path="/"
+				render={ props => {
+					return <HomeView />;
+				}}
+			/>
+		</Router>
+	);
 }
---
+++ src/components/HomeView.js
@@ -0,0 +1,16 @@
+/**
+ * FMC-frontend/src/components/HomeView.js
+ * 
+ * Landing page for Find My Course. Offers basic search functionality.
+ * 
+ * Author(s):
+ * 		Jerry Yu, Roxin Liu
+ */
+
+import React from "react";
+
+export default function HomeView() {
+	return (
+		<h1>Find my course home page.</h1>
+	);
+}
