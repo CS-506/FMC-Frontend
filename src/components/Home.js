5b920e8e99925c96a37cd011d2f099706d3ffec3
Roxin Liu <rliu227@wisc.edu>
2020-10-25 02:08:54 -0500

    Appbar and Cliped Drawer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
 import { 
-    Typography 
+    Typography,
+    Grid,
+
 } from "@material-ui/core/";
+import NavBar from "./NavBar";
+import TopBar from "./TopBar"
+
+const useStyles = makeStyles(theme => ({
+    navbar: {
+        flexGrow: 1,
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    contents: {
+        flexGrow: 1,
+        paddingTop: theme.spacing(10),
+        paddingLeft: theme.spacing(35),
+    },
+}));
 
 export default function Home() {
+    const classes = useStyles();
+
     return (
-        <Typography variant="h3">
-            Hello World. <br /> 
-            Welcome to Find My Course.
-        </Typography>
+        <div className = {classes.root}>
+            
+            <div>
+                {/*<NavBar className={classes.navbar} position="fixed" title="Find My Course" />*/}
+                <NavBar/>
+            </div>
+            <div>
+                
+            </div>
+            <main className = {classes.contents}>
+                <div>
+                    <Typography variant="h3">
+                        Hello World. <br /> 
+                        Welcome to Find My Course.
+                    </Typography>
+
+                </div>
+            </main>
+            
+            
+            
+        </div>
+        
     )
 }
