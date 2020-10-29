fd8991ed2df869082b18b81465f571b89d51b5bb
Roxin Liu <rliu227@wisc.edu>
2020-10-28 19:12:18 -0500

    Display dummy courses as search results

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import {
   BrowserRouter as Router,
   Route,
 } from "react-router-dom";
-import Home from "./components/Home"
+import SearchPage from "./components/Search"
 import Login from "./components/Login"
 
 export default function App() {
   return (
       <Router>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={SearchPage} />
         <Route exact path="/login" component={Login} />
       </Router>
     
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,10 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import { 
     Typography,
     Grid,
-
+    Paper,
+    List,
 } from "@material-ui/core/";
 import NavBar from "./NavBar";
-import TopBar from "./TopBar"
 
 const useStyles = makeStyles(theme => ({
     navbar: {
@@ -18,10 +18,57 @@ const useStyles = makeStyles(theme => ({
         paddingTop: theme.spacing(10),
         paddingLeft: theme.spacing(35),
     },
+    paper: {
+        margin: theme.spacing(2, 3),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'left',
+    },
 }));
 
 export default function Home() {
     const classes = useStyles();
+
+    // search keywords:
+    const [keywords, setKeyWords] = React.useState();
+    // course object:
+    const [course, setCourse] = React.useState();
+
+
+    // course list:
+    const courseList = [
+        {   
+            cid: 3214232,
+            title: "Introduction to Operating Systems",
+            dept: "Computer Sciences",
+            subject:" COMP SCI",
+            code: 537
+        },
+        {
+            cid: 3214233,
+            title: "Software Engineering",
+            dept: "Computer Sciences",
+            subject:" COMP SCI",
+            code: 506
+        },
+        {
+            cid: 3214234,
+            title: "Database Management",
+            dept: "Computer Sciences",
+            subject:" COMP SCI",
+            code: 564
+        },
+    ]
+
+    // PAGE STATES: 
+    const [isLoading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        console.log("React.useEffect");
+
+        
+        setLoading(false);
+    }, []);
 
     return (
         <div className = {classes.root}>
@@ -35,11 +82,19 @@ export default function Home() {
             </div>
             <main className = {classes.contents}>
                 <div>
-                    <Typography variant="h3">
-                        Hello World. <br /> 
-                        Welcome to Find My Course.
+                    <Typography variant="h5">
+                        Search Result for ...
                     </Typography>
-
+                    {courseList.map(c => (
+                        <Paper className={classes.paper} style={{ padding:10 }}>
+                            <Typography variant="h5">
+                                {c.title}
+                            </Typography>
+                            <Typography>
+                                {c.subject} {c.code}
+                            </Typography>
+                        </Paper>
+                    ))}
                 </div>
             </main>
             
