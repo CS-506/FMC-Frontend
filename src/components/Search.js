08abbe6238811fe26b32dbb43b62d0f42e93738c
Roxin Liu <rliu227@wisc.edu>
2020-11-04 02:04:38 -0600

    Display all search results stored in list. Cleared past obsolete codes in Search.js.

--- src/components/Search.js
+++ src/components/Search.js
@@ -32,39 +32,10 @@ const useStyles = makeStyles(theme => ({
 export default function Search(props) {
     const classes = useStyles();
 
-    // course list:
-    const initList = [
-        {   
-            cid: 3214232,
-            title: "Introduction to Operating Systems",
-            dept: "Computer Sciences",
-            subject:" COMP SCI",
-            code: 537
-        },
-        {
-            cid: 3214233,
-            title: "Software Engineering",
-            dept: "Computer Sciences",
-            subject:" COMP SCI",
-            code: 506
-        },
-        {
-            cid: 3214234,
-            title: "Database Management",
-            dept: "Computer Sciences",
-            subject:" COMP SCI",
-            code: 564
-        },
-    ];
-
     // search keywords:
     const [keyWord, setKeyWord] = React.useState("No keyWord");
-    // course list:
-    const [courseList, setList] = React.useState(initList);
-    // course object:
-    const [course, setCourse] = React.useState();
     // search result:
-    const [result, saveResult] = React.useState();
+    const [result, saveResult] = React.useState([]);
 
 
     //const [state, dispatch] = useContext(AppContext);
@@ -72,23 +43,7 @@ export default function Search(props) {
     const changeInputValue = (newValue) => {
         dispatch({ type: 'UPDATE_INPUT', data: newValue,});
     };
-    */
-
-    // Fetch one course info from backend given the course id:
-    const loadCourse = React.useCallback((courseId) => {
-        axios.get("/admin/course/get/" + courseId)
-            .then((res) => {
-                console.log(res.data);
-                setCourse(res.data);
-            })
-            .catch((err) => {
-                alert("Course Loading Error.");
-            });
-        
-        setList(courseList.concat(course));
-        
-    }, []);
-    
+    */    
 
     // Search course info from backend given the entry:
     const searchCourse = React.useCallback((currKeyWord) => {
@@ -114,58 +69,31 @@ export default function Search(props) {
         setKeyWord(currKeyWord);
         searchCourse(currKeyWord);
         setLoading(false);
-    }, [loadCourse]);
+    }, [setKeyWord, searchCourse]);
 
     
     function DisplaySearch() {
         return (
             <div>
-                {result ?
+                {result.map(resultItem => (
                     <Link to="/course" style={{ textDecoration: 'none' }}>
                         <Paper
                             className={classes.paper} 
                             style={{ padding:10, paddingLeft:20}}
                         >
                             <Typography variant="h5">
-                                {result[1][3]}
+                                {resultItem[3]}
                             </Typography>
                             <Typography variant="subtitle1">
-                                {result[1][1]} {result[1][2]}
+                                {resultItem[1]} {resultItem[2]}
                             </Typography>
                         </Paper>
                     </Link>
-                    :<Typography variant="h5">Search Loading...</Typography>
-                }
+                ))}
             </div>
         );
     }
-    function DisplayCourses() {
-        return (
-            isLoading ?
-            <Typography variant="h3">
-                Please wait...
-            </Typography>
-            :
-            <div >
-                {course ?
-                    <Link to="/course" style={{ textDecoration: 'none' }}>
-                        <Paper
-                            className={classes.paper} 
-                            style={{ padding:10 }}
-                        >
-                            <Typography variant="h5">
-                                {course.name}
-                            </Typography>
-                            <Typography>
-                                {course.subject} {course.code}
-                            </Typography>
-                        </Paper>
-                    </Link>
-                    :<Typography variant="h4">Loading...</Typography>
-                }
-            </div>
-        );
-    }
+    
     return (
         <div className = {classes.root}>
             
