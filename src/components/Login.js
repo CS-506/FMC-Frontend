1a53a046ffbd94be56de0647be524a5052544497
Jerry Yu <jyu283@wisc.edu>
2020-11-10 12:12:44 -0600

    indentation fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,41 +11,41 @@ import {
 import bgImage from "./img/login_bg.jpg";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        height: "100%",
-    },
-    desktop: {
-        backgroundColor: "transparent",
-        boxShadow: "none",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    window: {
-      maxWidth: "600px",
-    },
-    paper: {
-        margin: theme.spacing(12, 4),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    image: {
-        height: '100%',
-        width: "100%",
-        position: "fixed",
-        backgroundImage: `url(${bgImage})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundColor:
-          theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
+  root: {
+    height: "100%",
+  },
+  desktop: {
+    backgroundColor: "transparent",
+    boxShadow: "none",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  window: {
+    maxWidth: "600px",
+  },
+  paper: {
+    margin: theme.spacing(12, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  image: {
+    height: '100%',
+    width: "100%",
+    position: "fixed",
+    backgroundImage: `url(${bgImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
 }));
 
 
@@ -59,7 +59,7 @@ export default function Login(props) {
   const passwordHandler = event => setPassword(event.target.value);
 
   /* Page states */
-  const [showAlert, setShowAlert] = React.useState(false); 
+  const [showAlert, setShowAlert] = React.useState(false);
   const [alertSeverity, setAlertSeverity] = React.useState("info");
   const [alertText, setAlertText] = React.useState("");
 
@@ -75,101 +75,101 @@ export default function Login(props) {
 
   return (
     <div className={classes.root}>
-    <CssBaseline />
-    <Grid item className={classes.image}>
-    <div className={classes.desktop}>
-    <div className={classes.window}>
-      <Paper className={classes.paper}>
+      <CssBaseline />
+      <Grid item className={classes.image}>
+        <div className={classes.desktop}>
+          <div className={classes.window}>
+            <Paper className={classes.paper}>
 
-          <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log In
+              <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Log In
           </Typography>
-          <hr />
-          <br />
+                <hr />
+                <br />
 
-          <Collapse in={showAlert}>
-            <Alert
-              severity={alertSeverity}
-              action={
-                <IconButton
-                  aria-label="close"
-                  color="inherit"
-                  size="small"
-                  onClick={() => { setShowAlert(false); }}
-                >
-                  <CloseIcon fontSize="inherit" />
-                </IconButton>
-              }
-            >
-              {alertText}
-            </Alert>
-          </Collapse>
+                <Collapse in={showAlert}>
+                  <Alert
+                    severity={alertSeverity}
+                    action={
+                      <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"
+                        onClick={() => { setShowAlert(false); }}
+                      >
+                        <CloseIcon fontSize="inherit" />
+                      </IconButton>
+                    }
+                  >
+                    {alertText}
+                  </Alert>
+                </Collapse>
 
-          <br />
+                <br />
 
-          <form className={classes.form}>
-            <Grid container spacing={4}>
-              <Grid item sm={12}>
-                <TextField
-                  name="email"
-                  id="email"
-                  value={email}
-                  label="Email"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  onChange={emailHandler}
-                />
-              </Grid>
+                <form className={classes.form}>
+                  <Grid container spacing={4}>
+                    <Grid item sm={12}>
+                      <TextField
+                        name="email"
+                        id="email"
+                        value={email}
+                        label="Email"
+                        variant="outlined"
+                        required
+                        fullWidth
+                        onChange={emailHandler}
+                      />
+                    </Grid>
 
-              <Grid item sm={12}>
-                <TextField
-                  name="password"
-                  id="password"
-                  value={password}
-                  label="Password"
-                  type="password"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  onChange={passwordHandler}
-                />
-              </Grid>
+                    <Grid item sm={12}>
+                      <TextField
+                        name="password"
+                        id="password"
+                        value={password}
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        required
+                        fullWidth
+                        onChange={passwordHandler}
+                      />
+                    </Grid>
 
-              <Grid item sm={12} md={6}>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  className={classes.register}
-                  fullWidth
-                  href="/register"
-                >
-                  REGISTER
+                    <Grid item sm={12} md={6}>
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        className={classes.register}
+                        fullWidth
+                        href="/register"
+                      >
+                        REGISTER
                 </Button>
-              </Grid>
+                    </Grid>
 
-              <Grid item sm={12} md={6}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  className={classes.login}
-                  onClick={loginHandler}
-                  fullWidth
-                >
-                  LOG IN
+                    <Grid item sm={12} md={6}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.login}
+                        onClick={loginHandler}
+                        fullWidth
+                      >
+                        LOG IN
                 </Button>
-              </Grid>
-            </Grid>
-          </form>
+                    </Grid>
+                  </Grid>
+                </form>
+              </div>
+            </Paper>
           </div>
-      </Paper>
-    </div>
-    </div>
-    </Grid>
+        </div>
+      </Grid>
     </div>
   );
 }
