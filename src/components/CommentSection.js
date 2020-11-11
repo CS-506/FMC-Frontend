6d46f9eb1a67d4e2f5f665f4ea38980c515ee05d
Roxin Liu <rliu227@wisc.edu>
2020-11-11 01:36:17 -0600

    Comment component shown as paper.

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -14,10 +14,33 @@ const useStyles = makeStyles(theme => ({
 
 export default function CommentSection(props) {
   const classes = useStyles();
+  const [RC, setRC] = React.useState();
 
+  React.useEffect(() => {
+    setRC(props.rc);
+  }, [setRC]);
+  
   return (
-    <Typography className={classes.unit} variant="h5">
-      Comments
-    </Typography>
+    <Link 
+      to={{ pathname: 'course_' }}
+      
+      style={{ textDecoration: 'none' }}
+      key={user.RC.indexOf(rcItem)}
+    >
+      <Paper
+        className={classes.paper} 
+        style={{ padding:10, paddingLeft:20}}
+        md={3}
+      >
+        <Typography variant="subtitle1">
+            {rcItem.subject} {rcItem.code}<br/> 
+            {rcItem.name} 
+        </Typography>
+        <Typography>
+          Rating: {rcItem.rate} <br/> 
+          Comment: {rcItem.comment}
+        </Typography>
+      </Paper>
+    </Link>
   );
 }
